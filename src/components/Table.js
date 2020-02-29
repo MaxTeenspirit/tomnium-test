deb3508c78e7a1ed62d3f70a3486249ce62d59b8
Maksym Arabadzhyi <maximkaproduction1@gmail.com>
2020-02-29 11:45:43 +0200

    added some code styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,43 +2,43 @@ import React, { Component } from 'react';
 import './Table.css';
 
 class Table extends Component {
-    render() {
-        const state = this.props.state;
-        return (
-            <div className="Table">
-                {
-                    Object.keys(state).map((key, index) => (
-                        typeof state[key] !== 'object'
-                        ? <div key={index} className="Table__row">
-                            <div className="Table__cell Table__cell_yellow">
-                              {`${key}`}
-                            </div>
-                            <div className="Table__cell Table__cell_yellow">
-                              {`${state[key]}`}
-                            </div>
-                          </div>
-                        : 
-                          <div className="Table__rate-wrapper">
-                              <div className="Table__row Table__row_title">{key}</div>
-                            {
-                              Object.keys(state[key]).map((innerKey, index) => (
-                                <div key={index} className="Table__row">
-                                  <div className="Table__cell Table__cell_green">
-                                    {`${innerKey}`}
-                                  </div>
-                                  <div className="Table__cell Table__cell_green">
-                                    {`${state[key][innerKey]}`}
-                                  </div>
-                                </div>
-                                )
-                              )
-                            }
-                          </div>
-                    ))
-                }
-            </div>
-        )
-    }
+  render() {
+    const state = this.props.state;
+      return (
+        <div className="Table">
+          {
+            Object.keys(state).map((key, index) => (
+              typeof state[key] !== 'object'
+              ? <div key={index} className="Table__row">
+                  <div className="Table__cell Table__cell_yellow">
+                    {`${key}`}
+                  </div>
+                  <div className="Table__cell Table__cell_yellow">
+                    {`${state[key]}`}
+                  </div>
+                </div>
+              : 
+                <div key={index} className="Table__rate-wrapper">
+                  <div className="Table__row Table__row_title">{key}</div>
+                  {
+                    Object.keys(state[key]).map((innerKey, index) => (
+                      <div key={index} className="Table__row">
+                        <div className="Table__cell Table__cell_green">
+                          {`${innerKey}`}
+                        </div>
+                        <div className="Table__cell Table__cell_green">
+                          {`${state[key][innerKey]}`}
+                        </div>
+                      </div>
+                      )
+                    )
+                  }
+                </div>
+              ))
+            }
+        </div>
+    )
+  }
 }
 
 export default Table;
